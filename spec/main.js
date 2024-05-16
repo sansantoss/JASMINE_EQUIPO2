65748d598094d12c2dacbb6fa206e9fdd53d57dc
Santiago Santos <santiagosantos@Santiagos-MacBook-Pro-2.local>
2024-05-15 18:34:49 -0600

    Prueba 4, Optimizar, se construye el modelo a usar en las pruebas

--- spec/main.js
+++ spec/main.js
@@ -1,26 +1,18 @@
 describe('${User.name} Class', () => {
+	let model;
+	beforeEach(() => {
+		model = new User();
+    });
 	describe('default values', () => {
 		it('first name defaults to empty', () => {
-		//arrange
-		const data = { firstName: null };
-		//act
-		const model = new User(data);
 		//assert
 		expect(model.firstName).toBe('');
 	});
 	it('last name defaults to empty', () => {
-		//arrange
-		const data = { lastName: null };
-		//act
-		const model = new User(data);
 		//assert
 		expect(model.lastName).toBe('');
 	});
 	it('middle name defaults to empty', () => {
-		//arrange
-		const data = { middleName: null };
-		//act
-		const model = new User(data);
 		//assert
 		expect(model.middleName).toBe('');
 	});
