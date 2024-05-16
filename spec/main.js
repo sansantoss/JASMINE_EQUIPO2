ec7a1b75824be7539a43aea8b2f4f8dc9e00c3d4
Santiago Santos <santiagosantos@Santiagos-MacBook-Pro-2.local>
2024-05-15 19:40:31 -0600

    EJEMPLO3, toEqual

--- spec/main.js
+++ spec/main.js
@@ -7,5 +7,15 @@ describe('${User.name} Class', () => {
         model = new User();
     });
 	describe('Aditional Matcher Examples', () => {
+		it('gets full names pieces', () => {
+			const firstName = 'pepe';
+			const middleName = 'grillo';
+			const lastName = 'perez';
+			//act
+			model = new User(firstName, middleName, lastName);
+
+			//assert
+			expect(model.fullNamePieces).toEqual([firstName, middleName, lastName]);
+    });
 	});
 });
