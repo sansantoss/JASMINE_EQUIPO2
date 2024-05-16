ef8c3d2200c89f4576fef0229efda1b092d36389
Santiago Santos <santiagosantos@Santiagos-MacBook-Pro-2.local>
2024-05-15 18:59:14 -0600

    Prueba 5, nombre completo

--- spec/main.js
+++ spec/main.js
@@ -1,20 +1,49 @@
 describe('${User.name} Class', () => {
-	let model;
-	beforeEach(() => {
-		model = new User();
-    });
 	describe('default values', () => {
 		it('first name defaults to empty', () => {
+		//arange
+		const data = { firstName: null };
+		//act
+		const model = new User(data);
 		//assert
 		expect(model.firstName).toBe('');
-	});
+    });
 	it('last name defaults to empty', () => {
-		//assert
-		expect(model.lastName).toBe('');
+		//arange
+        const data = { lastName: null };
+        //act
+        const model = new User(data);
+        //assert
+        expect(model.lastName).toBe('');
+    });
+    it('middle name defaults to empty', () => {
+		//arange
+		const data = { middleName: null };
+        //act
+        const model = new User(data);
+        //assert
+        expect(model.middleName).toBe('');
+    });
+});
+describe('full name', () => {
+	beforeEach(() => {
+		model = new User({firstName: 'San', lastName: 'Santos'});
 	});
-	it('middle name defaults to empty', () => {
-		//assert
-		expect(model.middleName).toBe('');
+	it('middle initial when middleName is defined with first and last name', () => {
+		//arrange
+		model.middleName = 'Cruz';
+		 //act
+        const result = model.fullName;
+        //assert
+        expect(result).toBe('${model.firstName} ${model.middleName[0]}. ${model.lastName}');
+	});
+	it('when no middle name return just first and last', () => {
+		//arrange
+        model.middleName = '';
+         //act
+        const result = model.fullName;
+        //assert
+        expect(result).toBe('${model.firstName} ${model.lastName}');
 	});
 });
 });
--- spec/user.js
+++ spec/user.js
@@ -7,4 +7,10 @@ class User {
 	this.lastName = data.lastName || '';
 	this.middleName = data.middleName || '';
 	}
+    get fullName() {
+        if(this.middleName.length > 0){
+            return `${this.firstName} ${this.middleName} ${this.lastName}`;
+        }
+        return `${this.firstName} ${this.lastName}`;
+    }
 	}
