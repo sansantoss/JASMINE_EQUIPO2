5909907108ae1ca53b818ba73b82a2560f426d8f
Santiago Santos <santiagosantos@Santiagos-MacBook-Pro-2.local>
2024-05-15 18:20:37 -0600

    Prueba 3, Paso 4, first name defaults to empty

--- spec/main.js
+++ spec/main.js
@@ -1,21 +1,23 @@
-// Majority
-
-class Admin {
+class User {
 	firstName;
 	lastName;
 	middleName;
-	
 	constructor(data = {}){
 	this.firstName = data.firstName || '';
 	this.lastName = data.lastName || '';
 	this.middleName = data.middleName;
-
 	}
-}
+	}
 	/**
 	* Test Suite
 	*/
-describe(`${User.name} Class`, () => {
-});
-	
-	
+	describe(`${User.name} Class`, () => {
+	it('first name defaults to empty', () => {
+	// arrange
+	const data = { firstName: null };
+	// act
+	const model = new User(data);
+	// assert
+	expect(model.firstName).toBe('');
+	});
+	});
